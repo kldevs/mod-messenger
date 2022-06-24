883e448e914c8d22bce82c977db2be099398340a
unknown <kldevs@proton.me>
2022-06-24 00:16:20 -0400

    Seperate self messages. CSS makeover

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
-let displayName
+let displayName = localStorage.getItem('lsName') || 'Guest'
+
+let selfMessages = Array.from(document.querySelectorAll('.msgUserName')).filter(x=> x.innerHTML === displayName)
+selfMessages.forEach(x => x.parentNode.classList.add('self'))
 
 function printStatus(){
   document.querySelector('#msgLoginStatus').innerHTML = `Welcome, <a href="/login">${displayName || "Guest"}</a>`
@@ -9,5 +12,8 @@ if(localStorage.getItem('lsName')){
   displayName = localStorage.getItem('lsName')
   printStatus()
 }else{
+  displayName = 'Guest'
   printStatus()
 }
+
+document.querySelector('.messages').scrollTop = document.querySelector('.messages').scrollHeight
